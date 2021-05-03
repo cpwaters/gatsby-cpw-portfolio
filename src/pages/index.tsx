318f413806c0b4ae9bd8ef8d286eb64a4e216d7b
Chris Waters <cp-waters@hotmail.co.uk>
2021-05-03 13:38:08 +0100

    install of sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
-import React from 'react';
+import React from 'react'
+import Navbar from '../components/Navbar'
 
-export default () => <div>Hello world!</div>;
+export default function Home() {
+  return (
+    <section>
+      <Navbar/>
+      <h1>Welcome [0] & *</h1>
+      <h3>Design & Development</h3>
+    </section>
+  )
+}
+
---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <div>
+      <h1>Design & Development</h1>
+      <p>All Work</p>
+    </div>
+  )
+}
