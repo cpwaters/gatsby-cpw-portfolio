e90a1d26b34112d010bdb31ca5cd788a3ddacee8
Chris Waters <cp-waters@hotmail.co.uk>
2021-05-23 13:42:00 +0100

    Adding imgs, plugins & css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,35 @@
+import { graphql, Link } from 'gatsby';
 import React from 'react';
 import Layout from '../components/Layout';
+import styles from '../styles/home.module.css';
 
-export default function Home() {
+export default function Home({data}) {
+  console.log(data);
+  const { title, description } = data.site.siteMetadata;
   return (
     <Layout>
-      <section>
-        <h1>Welcome [0] & *</h1>
-        <h3>Design & Development</h3>
+      <section className={styles.header}>
+        <div>
+          <h2>Welcome [0] & *</h2>
+          <h3>Design & Development</h3>
+          <Link className={styles.btn} to="/projects">My Projects</Link>
+        </div>
+        <img src="/dev.png" alt="developer" style={{ maxWidth: '100%' }} />
+        <p>{title} - {description}</p>
       </section>
     </Layout>
   );
 }
+
+/* graphQL page query */
+export const query = graphql`
+query SiteData {
+  site {
+    siteMetadata {
+      copyright
+      description
+      title
+    }
+  }
+}
+`
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import styles from '../../styles/projects.module.css';
 
 export default function Projects() {
   return (
     <Layout>
-      <div>
-        <h1>Design & Development</h1>
-        <p>All Work</p>
+      <div className={styles.portfolio}>
+        <h2>Design & Development</h2>
+        <h3>All Work</h3>
       </div>
     </Layout>
   );
